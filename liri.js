ae627f9aee5338501dcd36f826c4eb4ff5ae2a29
Jan Balangue <jantonio.balangue@gmail.com>
2018-04-05 00:16:12 -0700

    Modified liri.js to use more string interpolation

--- liri.js
+++ liri.js
@@ -17,7 +17,7 @@ function processTweets() {
             console.log("Tweets:");
             for (var i = 0; i < 20; i++) {
                 console.log(`${tweets[i].created_at}: ${tweets[i].text}`);
-                appendToFile(tweets[i].created_at + ": " + tweets[i].text + "\r\n");
+                appendToFile(`${tweets[i].created_at}: ${tweets[i].text}\r\n`);
             }
             appendToFile("---------------------------\r\n");
         }
@@ -33,11 +33,11 @@ function processSpotifyRequest(arg) {
                 console.log(`Song Name: ${data.tracks.items[0].name}`);
                 console.log(`Preview URL: ${data.tracks.items[0].preview_url}`);
                 console.log(`Album: ${data.tracks.items[0].album.name}`);
-                appendToFile("Artist: " + data.tracks.items[0].artists[0].name + "\r\n");
-                appendToFile("Song Name: " + data.tracks.items[0].name + "\r\n");
-                appendToFile("Preview URL: " + data.tracks.items[0].preview_url + "\r\n");
-                appendToFile("Album: " + data.tracks.items[0].album.name + "\r\n");
-                appendToFile("---------------------------\r\n");
+                appendToFile(`Artist: ${data.tracks.items[0].artists[0].name}\r\n`);
+                appendToFile(`Song Name: ${data.tracks.items[0].name}\r\n`);
+                appendToFile(`Preview URL: ${data.tracks.items[0].preview_url}\r\n`);
+                appendToFile(`Album: ${data.tracks.items[0].album.name}\r\n`);
+                appendToFile(`---------------------------\r\n`);
             }
         });
     } else {
@@ -49,11 +49,11 @@ function processSpotifyRequest(arg) {
                 console.log(`Song Name: ${data.tracks.items[5].name}`);
                 console.log(`Preview URL: ${data.tracks.items[5].preview_url}`);
                 console.log(`Album: ${data.tracks.items[5].album.name}`);
-                appendToFile("Artist: " + data.tracks.items[5].artists[0].name + "\r\n");
-                appendToFile("Song Name: " + data.tracks.items[5].name + "\r\n");
-                appendToFile("Preview URL: " + data.tracks.items[5].preview_url + "\r\n");
-                appendToFile("Album: " + data.tracks.items[5].album.name + "\r\n");
-                appendToFile("---------------------------\r\n");
+                appendToFile(`Artist: ${data.tracks.items[5].artists[0].name}\r\n`);
+                appendToFile(`Song Name: ${data.tracks.items[5].name}\r\n`);
+                appendToFile(`Preview URL: ${data.tracks.items[5].preview_url}\r\n`);
+                appendToFile(`Album: ${data.tracks.items[5].album.name}\r\n`);
+                appendToFile(`---------------------------\r\n`);
             }
         });
     }
@@ -73,15 +73,15 @@ function processOMDBRequest(arg) {
         console.log(`Language of the movie: ${JSON.parse(body).Language}`);
         console.log(`Plot synopsis: ${JSON.parse(body).Plot}`);
         console.log(`Actors: ${JSON.parse(body).Actors}`);
-        appendToFile("Movie title: " + JSON.parse(body).Title + "\r\n");
-        appendToFile("Year: " + JSON.parse(body).Year + "\r\n");
-        appendToFile("IMDB Rating: " + JSON.parse(body).Ratings[0].Value + "\r\n");
-        appendToFile("Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value + "\r\n");
-        appendToFile("Country where the movie was produced: " + JSON.parse(body).Country + "\r\n");
-        appendToFile("Language of the movie: " + JSON.parse(body).Language + "\r\n");
-        appendToFile("Plot synopsis: " + JSON.parse(body).Plot + "\r\n");
-        appendToFile("Actors: " + JSON.parse(body).Actors + "\r\n");
-        appendToFile("---------------------------\r\n");
+        appendToFile(`Movie title: ${JSON.parse(body).Title}\r\n`);
+        appendToFile(`Year: ${JSON.parse(body).Year}\r\n`);
+        appendToFile(`IMDB Rating: ${JSON.parse(body).Ratings[0].Value}\r\n`);
+        appendToFile(`Rotten Tomatoes Rating: ${JSON.parse(body).Ratings[1].Value}\r\n`);
+        appendToFile(`Country where the movie was produced: ${JSON.parse(body).Country}\r\n`);
+        appendToFile(`Language of the movie: ${JSON.parse(body).Language}\r\n`);
+        appendToFile(`Plot synopsis: ${JSON.parse(body).Plot}\r\n`);
+        appendToFile(`Actors: ${JSON.parse(body).Actors}\r\n`);
+        appendToFile(`---------------------------\r\n`);
     });
     //  Title of the movie.
     //  Year the movie came out.
